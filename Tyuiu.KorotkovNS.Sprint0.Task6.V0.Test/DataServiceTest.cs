929f3b3a22da25bfd71061c8ef825c8fa0d0f3a2
tresh78805@gmail.com <tresh78805@gmail.com>
2024-09-16 12:01:51 +0500

    Task6

---
+++ Tyuiu.KorotkovNS.Sprint0.Task6.V0.Test/DataServiceTest.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.KorotkovNS.Sprint0.Task6.V0.Lib;
+
+namespace Tyuiu.KorotkovNS.Sprint0.Task6.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionArrayVaid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.AdditionArray(numbers);
+            Assert.AreEqual(15, res);
+        }
+        [TestMethod]
+        public void CheckedSubtractionArrayVaid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.SubtractionArray(numbers);
+            Assert.AreEqual(-15, res);
+        }
+        [TestMethod]
+        public void CheckedMultiplicationArrayVaid()
+        {
+            var numbers = new int[] { 1, 2, 3, 4, 5 };
+            var res = DataService.MultiplicationArray(numbers);
+            Assert.AreEqual(120, res);
+        }
+    }
+}
